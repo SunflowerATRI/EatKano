4236a19b484cc63350b80eecca7d8554d7dc91c7
SunflowerATRI <103645545+SunflowerATRI@users.noreply.github.com>
2022-04-14 21:31:26 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -466,8 +466,8 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
 
         if (cps <= 5) return '装蒜呢';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
+        if (cps <= 8) return '小破车子';
+        if (cps <= 10)  return '大破车子';
         if (cps <= 15) return '您';
         return '人？';
     }
